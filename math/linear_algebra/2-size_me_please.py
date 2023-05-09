84583e7e9eee681a047eeadf7aa1d1840b4a87ea
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 10:24:04 -0500

    well this was embarassing XD

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,16 +5,17 @@ Module that calculates the shape of a matrix
 Return the shape as a list of integers
 
 If the matrix is not a list of lists, return None... since is no a matrix XD
-
-Args:
-matrix (list): A nested list representing the matrix
-
-Returns:
-list: A list of integers representing the dimensions of the matrix
 """
 
 
 def matrix_shape(matrix):
+    """
+    Args:
+    matrix (list): A nested list representing the matrix
+
+    Returns:
+    list: A list of integers representing the dimensions of the matrix
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
