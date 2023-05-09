ed1c4fef4912d02070c00c8f7e71ed5a06321510
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 10:09:16 -0500

    interesting

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that calculates the shape of a matrix"""
+
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
