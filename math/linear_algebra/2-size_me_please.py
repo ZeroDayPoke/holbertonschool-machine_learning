e76436cca9a61e1156b6c7e8f1790e2763035eea
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 10:12:43 -0500

    more docs?

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-"""Module that calculates the shape of a matrix"""
+"""
+Module that calculates the shape of a matrix
+
+Return the shape as a list of integers
+
+If the matrix is not a list of lists, return None... since is no a matrix XD
+"""
+
 
 def matrix_shape(matrix):
     shape = []
