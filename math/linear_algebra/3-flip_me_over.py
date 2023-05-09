243f86ac50cd9812d2220c38d808cc6b43a63b05
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 10:36:46 -0500

    uwu docs

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,5 +6,11 @@ Function that returns the transpose of a 2D matrix
 
 
 def matrix_transpose(matrix):
-    """"""
+    """
+    Function that returns the transpose of a 2D matrix
+    Args:
+        matrix: list of lists whose transpose should be returned
+    Returns:
+        The tpose of matrix
+    """
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
