e3ebb7d0cbca7584233ee16ae0de029fd4071d0c
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 19:23:08 -0500

    lololol

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that transposes a numpy.ndarray"""
+
+
+def np_transpose(matrix):
+    """
+    tpoze a numpy.ndarray
+    Args:
+        matrix (numpy.ndarray): matrix to transpose
+    Returns:
+        numpy.ndarray: transposed matrix
+    what was the point of the previous exercise?
+    if this functionality is built in?
+    """
+    return matrix.T
