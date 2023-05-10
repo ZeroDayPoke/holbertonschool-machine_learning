13a5db86155bf60841aa5201ba5cb696669287b8
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 19:34:44 -0500

    gg I guess... anticlimactic

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    man it's disappointing I can't make a
+    Pistol Pete joke in this one....
+    """
+    return np.matmul(mat1, mat2)
