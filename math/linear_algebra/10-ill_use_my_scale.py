2206127b947f3875fd20c27ffe5582c4a63aafe2
ZeroDayPoke <cstampok@gmail.com>
2023-05-09 19:18:56 -0500

    is this a trick question?

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    there's no way it can be this easy right?
+    Args:
+        matrix (numpy.ndarray): matrix to calculate the shape of
+    Returns:
+        tuple: shape of matrix
+    """
+    return matrix.shape
