3182a556747a934fd5b1a437d576a2a031756986
ZeroDayPoke <cstampok@gmail.com>
2023-05-16 17:21:43 -0500

    ah, it wants the sum of squares

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,4 +6,4 @@ def summation_i_squared(n):
     """Returns the sum of i^2 i = 1 to n."""
     if not isinstance(n, int) or n < 1:
         return None
-    return sum(i**2 for i in range(1, n+1))
+    return n * (n + 1) * (2 * n + 1) // 6
