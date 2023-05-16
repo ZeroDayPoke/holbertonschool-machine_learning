5663ac5c23002ef913962dc1009d4c0734f0f492
ZeroDayPoke <cstampok@gmail.com>
2023-05-16 17:18:27 -0500

    add doc strings

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,9 @@
 #!usr/bin/env python3
+"""Returns the sum of i^2 for i = 1 to n."""
+
 
 def summation_i_squared(n):
+    """Returns the sum of i^2 for i = 1 to n."""
     if not isinstance(n, int) or n < 1:
         return None
     return sum(i**2 for i in range(1, n+1))
