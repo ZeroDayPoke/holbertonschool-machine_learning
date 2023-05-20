d0112405b0f72f08e87b7da0d19a43ff35e6a334
ZeroDayPoke <cstampok@gmail.com>
2023-05-20 17:07:45 -0500

    test

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -3,6 +3,19 @@
 
 
 class Binomial:
+    """
+    Binomial distribution
+        Args:
+            data (list): List of the data to be
+            used to estimate the distribution
+            n (int): Number of Bernoulli trials
+            p (float): Probability of a “success”
+        Raises:
+            ValueError: If n is not a positive value
+            ValueError: If p is not a valid probability
+            TypeError: If data is not a list
+            ValueError: If data does not contain at least two data points
+    """
     def __init__(self, data=None, n=1, p=0.5):
         if data is None:
             if n <= 0:
@@ -23,7 +36,7 @@ class Binomial:
             self.p = total_successes / total_trials
 
             # Calculate n by rounding to the nearest integer
-            self.n = round(total_trials / self.p)
+            self.n = round(total_trials * self.p)
 
             # Recalculate p using the rounded n
             self.p = total_successes / self.n
