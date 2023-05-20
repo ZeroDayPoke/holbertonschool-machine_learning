b6ba93c283d4816fbdfb6fe400d99544cdbe2bf7
ZeroDayPoke <cstampok@gmail.com>
2023-05-20 17:29:48 -0500

    test

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -19,24 +19,16 @@ class Binomial:
     def __init__(self, data=None, n=1, p=0.5):
         if data is None:
             if n <= 0:
-                raise ValueError("n must be a positive value")
-            if not 0 < p < 1:
-                raise ValueError("p must be greater than 0 and less than 1")
+                raise ValueError('n must be a positive value')
+            if p <= 0 or p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
             self.n = int(n)
             self.p = float(p)
         else:
             if not isinstance(data, list):
-                raise TypeError("data must be a list")
+                raise TypeError('data must be a list')
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-
-            # Calculate p from data
-            total_successes = sum(data)
-            total_trials = len(data)
-            self.p = total_successes / total_trials
-
-            # Calculate n by rounding to the nearest integer
-            self.n = round(total_trials * self.p)
-
-            # Recalculate p using the rounded n
-            self.p = total_successes / self.n
+                raise ValueError('data must contain multiple values')
+            self.p = sum(data)/len(data)/n
+            self.n = round(sum(data)/self.p)
+            self.p = sum(data)/len(data)/self.n
