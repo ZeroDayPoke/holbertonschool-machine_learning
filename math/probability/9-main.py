a004ec84d54ee50c64b57cbdcca92f1f517d385a
ZeroDayPoke <cstampok@gmail.com>
2023-05-24 21:20:11 -0500

    updated normal test file

--- math/probability/9-main.py
+++ math/probability/9-main.py
@@ -5,8 +5,17 @@ Normal = __import__('normal').Normal
 
 np.random.seed(0)
 data = np.random.normal(70, 10, 100).tolist()
+
 n1 = Normal(data)
-print('PHI(90):', n1.cdf(90))
+print('===== NORMAL ===== DATA GIVEN =====')
+print('n1 cdf(90):', n1.cdf(90))
+print('n1 mu:', n1.mean)
+print('n1 sigma:', n1.stddev)
+print('n1 pdf(90):', n1.pdf(90))
 
 n2 = Normal(mean=70, stddev=10)
-print('PHI(90):', n2.cdf(90))
+print('===== NORMAL ===== DATA NOT GIVEN =====')
+print('n2 cdf(90):', n2.cdf(90))
+print('n2 mu:', n2.mean)
+print('n2 sigma:', n2.stddev)
+print('n2 pdf(90):', n2.pdf(90))
