b53fdd13b35babf96dddb3c3c1880d3c29770dfa
ZeroDayPoke <cstampok@gmail.com>
2023-06-07 02:11:47 -0500

    test 17 18

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Class DeepNeuralNetwork"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network"""
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or not layers:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.__L):
+            self.__weights['W' + str(i+1)] = np.random.randn(
+                layers[i], nx) * np.sqrt(2/nx)
+            self.__weights['b' + str(i+1)] = np.zeros((layers[i], 1))
+            nx = layers[i]
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
