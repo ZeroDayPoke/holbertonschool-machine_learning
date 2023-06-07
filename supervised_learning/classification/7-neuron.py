5867b75c498974585c1377fea7d0b3ce6ebb1c54
ZeroDayPoke <cstampok@gmail.com>
2023-06-06 21:19:06 -0500

    test 7

---
+++ supervised_learning/classification/7-neuron.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Class Neuron"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError('nx must be a integer')
+        if nx < 1:
+            raise ValueError('nx must be positive')
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions"""
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
+        m = X.shape[1]
+        dz = A - Y
+        dw = 1 / m * np.matmul(X, dz.T)
+        db = 1 / m * np.sum(dz)
+        self.__W = self.__W - alpha * dw.T
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True, step=100):
+        """Trains the neuron"""
+        if type(iterations) != int:
+            raise TypeError('iterations must be an integer')
+        if iterations <= 0:
+            raise ValueError('iterations must be a positive integer')
+        if type(alpha) != float:
+            raise TypeError('alpha must be a float')
+        if alpha <= 0:
+            raise ValueError('alpha must be positive')
+        if verbose is True or graph is True:
+            if type(step) != int:
+                raise TypeError('step must be an integer')
+            if step <= 0 or step > iterations:
+                raise ValueError('step must be positive and <= iterations')
+
+        costs = []
+        for i in range(iterations + 1):
+            A = self.forward_prop(X)
+            if verbose and i % step == 0:
+                print("Cost after {} iterations: {}".format(i, self.cost(Y, A)))
+            if i != iterations:
+                self.gradient_descent(X, Y, A, alpha)
+            if graph and i % step == 0:
+                costs.append(self.cost(Y, self.A))
+        
+        if graph:
+            plt.plot(np.arange(0, iterations + 1, step), costs)
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
+            plt.show()
+
+        return self.evaluate(X, Y)
