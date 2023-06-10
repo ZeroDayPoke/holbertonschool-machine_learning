e676bfba22b399135dc63402ea90698c2edd93f1
ZeroDayPoke <cstampok@gmail.com>
2023-06-10 11:20:13 -0500

    test - collab output working

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Tensorflow module"""
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    Function to create placeholders for input data and labels for the neural network.
+    Arguments:
+    nx: int, the number of feature columns in our data
+    classes: int, the number of classes in our classifier
+
+    Returns:
+    x: placeholder for the input data to the neural network
+    y: placeholder for the one-hot labels for the input data
+    """
+
+    x = tf.placeholder(dtype=tf.float32, shape=[None, nx], name="x")
+    y = tf.placeholder(dtype=tf.float32, shape=[None, classes], name="y")
+
+    return x, y
