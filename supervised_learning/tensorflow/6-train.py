5cfdb7004f6de916e00cd9771649270e9bf3e2d1
ZeroDayPoke <cstampok@gmail.com>
2023-06-10 13:23:06 -0500

    test

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -2,6 +2,11 @@
 """Tensorflow module"""
 
 import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid,
@@ -10,11 +15,6 @@ def train(X_train, Y_train, X_valid, Y_valid,
     """
     Builds, trains, and saves a neural network classifier
     """
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = __import__('0-create_placeholders').create_placeholders
-    create_train_op = __import__('5-create_train_op').create_train_op
-    forward_prop = __import__('2-forward_prop').forward_prop
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
@@ -43,6 +43,4 @@ def train(X_train, Y_train, X_valid, Y_valid,
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
-        save_path = saver.save(sess, save_path)
-
-    return save_path
+        return saver.save(sess, save_path)
