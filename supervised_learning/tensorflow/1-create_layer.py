1b2e15d2185a78512b5d4d3754213f80ad8fb2f2
ZeroDayPoke <cstampok@gmail.com>
2023-06-10 11:32:19 -0500

    task 1 test - collab pass

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Tensorflow module"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Function to create a layer for the neural network.
+
+    Arguments:
+    prev: tensor, output of the previous layer
+    n: int, number of nodes in the layer to create
+    activation: activation function that the layer should use
+
+    Returns:
+    layer: tensor, output of the layer
+    """
+
+    initializer = tf.contrib.layers.\
+        variance_scaling_initializer(mode="fan_avg")
+
+    layer = tf.layers.dense(inputs=prev,
+                            units=n,
+                            activation=activation,
+                            kernel_initializer=initializer,
+                            name="layer")
+
+    return layer
