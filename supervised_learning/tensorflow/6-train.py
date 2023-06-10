300bafc222e45e12c3fdd739b4beb7bf80a7e24c
ZeroDayPoke <cstampok@gmail.com>
2023-06-10 12:56:30 -0500

    sandbox test fail

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Tensorflow module"""
+
+import tensorflow as tf
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """
+    Builds, trains, and saves a neural network classifier
+    """
+    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+    calculate_loss = __import__('4-calculate_loss').calculate_loss
+    create_placeholders = __import__('0-create_placeholders').create_placeholders
+    create_train_op = __import__('5-create_train_op').create_train_op
+    forward_prop = __import__('2-forward_prop').forward_prop
+    
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+    
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations + 1):
+            t_cost, t_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
+            v_cost, v_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+            
+            if i % 100 == 0 or i == iterations:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(t_cost))
+                print("\tTraining Accuracy: {}".format(t_accuracy))
+                print("\tValidation Cost: {}".format(v_cost))
+                print("\tValidation Accuracy: {}".format(v_accuracy))
+
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+                
+        save_path = saver.save(sess, save_path)
+        
+    return save_path
