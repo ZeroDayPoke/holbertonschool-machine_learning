5f493bc203e452a042c02700696392a8bba9526b
ZeroDayPoke <cstampok@gmail.com>
2023-06-10 12:39:35 -0500

    refactor based on sandbox output

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -19,8 +19,8 @@ def create_layer(prev, n, activation, i):
 
     initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
-    activation_name = activation.__name__ if activation is not None else "None"
-    layer_name = "layer" + str(i) + "_" + activation_name
+    activation_name = activation.__name__ if activation is not None else ""
+    layer_name = "layer" + "_" + str(i) + activation_name
 
     layer = tf.layers.dense(inputs=prev,
                             units=n,
