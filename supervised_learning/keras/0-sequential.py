78f3d2e5d5a2b31477b36e96ab521863f604afdf
ZeroDayPoke <cstampok@gmail.com>
2023-07-06 18:30:23 -0500

    test zero

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-"""Keras module"""
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.regularizers import l2
+"""Function that builds a neural network with the Keras library"""
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """builds a neural network with the Keras library"""
-    model = Sequential()
+    """Function that builds a neural network with the Keras library"""
+    model = K.models.Sequential()
     for i in range(len(layers)):
         if i == 0:
             model.add(
-                Dense(
+                K.layers.Dense(
                     layers[i],
                     input_dim=nx,
                     activation=activations[i],
-                    kernel_regularizer=l2(lambtha)))
+                    kernel_regularizer=K.regularizers.l2(lambtha)))
         else:
             model.add(
-                Dense(
+                K.layers.Dense(
                     layers[i],
                     activation=activations[i],
-                    kernel_regularizer=l2(lambtha)))
+                    kernel_regularizer=K.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
-            model.add(Dropout(1 - keep_prob))
+            model.add(K.layers.Dropout(1 - keep_prob))
     return model
