7dd75c6af248a397c7417653382115a71a9c99ec
ZeroDayPoke <cstampok@gmail.com>
2023-07-06 18:22:29 -0500

    test five

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -10,36 +10,32 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        pad_h = max((kh - 1) // 2, 0)
-        pad_w = max((kw - 1) // 2, 0)
+        pad_h = ((((h - 1) * sh) + kh - h) // 2) + 1
+        pad_w = ((((w - 1) * sw) + kw - w) // 2) + 1
     elif padding == 'valid':
         pad_h = 0
         pad_w = 0
     else:
         pad_h, pad_w = padding
 
+    output_h = (h - kh + 2 * pad_h) // sh + 1
+    output_w = (w - kw + 2 * pad_w) // sw + 1
+
     padded_images = np.pad(
         images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), 'constant')
-
-    output_h = (h + 2 * pad_h - kh) // sh + 1
-    output_w = (w + 2 * pad_w - kw) // sw + 1
 
     output = np.zeros((m, output_h, output_w, nc))
 
     for n in range(nc):
-        for x in range(0, output_h):
-            for y in range(0, output_w):
-                output[:,
-                       x,
-                       y,
-                       n] = (padded_images[:,
-                                           x * sh:x * sh + kh,
-                                           y * sw:y * sw + kw,
-                                           :] * kernels[:,
-                                                        :,
-                                                        :,
-                                                        n]).sum(axis=(1,
-                                                                      2,
-                                                                      3))
+        for x in range(output_h):
+            for y in range(output_w):
+                output[:, x, y, n] = (padded_images[:, x *
+                                                    sh:x *
+                                                    sh +
+                                                    kh, y *
+                                                    sw:y *
+                                                    sw +
+                                                    kw, :] *
+                                      kernels[..., n]).sum(axis=(1, 2, 3))
 
     return output
