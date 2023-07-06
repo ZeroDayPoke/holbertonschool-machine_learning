376f4b404ad8761a2bf204b73dc4d12b1cd1b2bf
ZeroDayPoke <cstampok@gmail.com>
2023-07-06 18:19:15 -0500

    test three

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -16,19 +16,19 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        pad_h = kh // 2
-        pad_w = kw // 2
+        pad_h = ((((h - 1) * sh) + kh - h) // 2) + 1
+        pad_w = ((((w - 1) * sw) + kw - w) // 2) + 1
     elif padding == 'valid':
         pad_h = 0
         pad_w = 0
     else:
         pad_h, pad_w = padding
 
+    output_h = (h - kh + 2 * pad_h) // sh + 1
+    output_w = (w - kw + 2 * pad_w) // sw + 1
+
     padded_images = np.pad(
         images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), 'constant')
-
-    output_h = (h + 2 * pad_h - kh) // sh + 1
-    output_w = (w + 2 * pad_w - kw) // sw + 1
 
     output = np.zeros((m, output_h, output_w))
 
