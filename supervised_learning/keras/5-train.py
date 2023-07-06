39bd8a7b734dbaf4021a9a239629600adf37e7da
ZeroDayPoke <cstampok@gmail.com>
2023-07-06 18:45:28 -0500

    test 5

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Train"""
+import tensorflow.keras as K
+
+
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        validation_data=None,
+        verbose=True,
+        shuffle=False):
+    """Function that trains a model using mini-batch gradient descent"""
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
+    return history
