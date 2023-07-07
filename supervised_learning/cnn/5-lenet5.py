23791e94423e6aaa6a30e71997b0fde9fd49247a
ZeroDayPoke <cstampok@gmail.com>
2023-07-07 06:42:03 -0500

    test 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -3,64 +3,42 @@
 Module to build, train, and validate a modified LeNet-5
 neural network model in keras
 """
-
 import tensorflow.keras as K
 
 
 def lenet5(X):
     """
-    Builds, trains, and validates a modified LeNet-5
-    neural network model in keras
-
-    parameters:
-        X [K.Input of shape (m, 28, 28, 1)]:
-            contains the input images for the network
-            m: number of images
-
-    The model should consist of the following layers in order:
-        - Convolutional layer with 6 kernels of shape 5x5 with same padding
-        - Max pooling layer with kernels of shape 2x2 with 2x2 strides
-        - Convolutional layer with 16 kernels of shape 5x5 with valid padding
-        - Max pooling layer with kernels of shape 2x2 with 2x2 strides
-        - Fully connected layer with 120 nodes
-        - Fully connected layer with 84 nodes
-        - Fully connected softmax output layer with 10 nodes
-
-    All layers requiring initialization should initialize their kernels with
-    the he_normal initialization method
-    All hidden layers requiring activation
-    should use the relu activation function
-
-    returns:
-        a K.Model compiled to use Adam optimization
-        (with default hyperparameters) and accuracy metrics
+    Function that builds a modified version of the LeNet-5
+    architecture using keras
+    Arguments:
+     - X is a K.Input of shape (m, 28, 28, 1) containing the input images
+        for the network
+        * m is the number of images
+    Returns:
+        A K.Model compiled to use Adam optimization (with default
+        hyperparameters) and accuracy metrics
     """
     init = K.initializers.he_normal(seed=None)
 
-    model = K.models.Sequential()
+    conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
+                            activation='relu', kernel_initializer=init)(X)
+    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
-    model.add(K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-                              activation='relu', kernel_initializer=init,
-                              input_shape=(28, 28, 1)))
+    conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
+                            activation='relu', kernel_initializer=init)(pool1)
+    pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    flatten = K.layers.Flatten()(pool2)
 
-    model.add(K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
-                              activation='relu', kernel_initializer=init))
+    fc1 = K.layers.Dense(units=120, activation='relu',
+                         kernel_initializer=init)(flatten)
+    fc2 = K.layers.Dense(units=84, activation='relu',
+                         kernel_initializer=init)(fc1)
 
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    logits = K.layers.Dense(units=10, kernel_initializer=init)(fc2)
+    y_pred = K.layers.Activation('softmax')(logits)
 
-    model.add(K.layers.Flatten())
-
-    model.add(K.layers.Dense(units=120, activation='relu',
-                             kernel_initializer=init))
-
-    model.add(K.layers.Dense(units=84, activation='relu',
-                             kernel_initializer=init))
-
-    model.add(K.layers.Dense(units=10, activation='softmax',
-                             kernel_initializer=init))
-
+    model = K.models.Model(inputs=X, outputs=y_pred)
     model.compile(optimizer=K.optimizers.Adam(),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
