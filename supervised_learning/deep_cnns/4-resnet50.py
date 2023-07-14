f00e14362090533b967394ebd8a3c2019344baca
Chris Stamper <cstampok@gmail.com>
2023-07-14 13:54:11 -0500

    test four

---
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""DCNN - ResNet-50"""
+
+import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
+
+
+def resnet50():
+    """ResNet-50"""
+    X_input = K.Input(shape=(224, 224, 3))
+
+    # Stage 1
+    X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
+                        kernel_initializer='he_normal')(X_input)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = projection_block(X, [64, 64, 256], s=1)
+    X = identity_block(X, [64, 64, 256])
+    X = identity_block(X, [64, 64, 256])
+
+    # Stage 3
+    X = projection_block(X, [128, 128, 512])
+    X = identity_block(X, [128, 128, 512])
+    X = identity_block(X, [128, 128, 512])
+    X = identity_block(X, [128, 128, 512])
+
+    # Stage 4
+    X = projection_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+
+    # Stage 5
+    X = projection_block(X, [512, 512, 2048])
+    X = identity_block(X, [512, 512, 2048])
+    X = identity_block(X, [512, 512, 2048])
+
+    # AVGPOOL
+    X = K.layers.AveragePooling2D(pool_size=(2, 2))(X)
+
+    # output layer
+    X = K.layers.Flatten()(X)
+    X = K.layers.Dense(units=1000,
+                       activation='softmax',
+                       kernel_initializer='he_normal')(X)
+
+    # Create model
+    model = K.models.Model(inputs=X_input,
+                           outputs=X, name='ResNet50')
+
+    return model
