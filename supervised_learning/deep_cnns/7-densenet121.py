9510eba76f8736ec2d2b10f8a3ef498e82f4b871
Chris Stamper <cstampok@gmail.com>
2023-07-14 15:08:15 -0500

    test seven

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -6,7 +6,7 @@ import tensorflow.keras as K
 
 def densenet121(growth_rate=32, compression=1.0):
     """Builds the DenseNet-121 architecture"""
-    # Import necessary functions
+
     dense_block = __import__('5-dense_block').dense_block
     transition_layer = __import__('6-transition_layer').transition_layer
 
