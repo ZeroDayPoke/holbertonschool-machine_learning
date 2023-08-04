55695e31268b90f79bfb37570ae71a8e64e29d09
Chris Stamper <cstampok@gmail.com>
2023-08-04 14:37:52 -0500

    placeholders filled in

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -6,16 +6,17 @@ import numpy as np
 
 
 class Yolo:
-    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """_summary_
+    """
+    Yolo v3 algorithm to perform object detection.
 
-        Args:
-            model_path (_type_): _description_
-            classes_path (_type_): _description_
-            class_t (_type_): _description_
-            nms_t (_type_): _description_
-            anchors (_type_): _description_
-        """
+    Attributes:
+        model (Keras model): The Darknet Keras model.
+        class_names (list): A list of the class names for the model.
+        class_t (float): The box score threshold for the initial filtering step.
+        nms_t (float): The IOU threshold for non-max suppression.
+        anchors (numpy.ndarray): The anchor boxes.
+    """
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = [line.strip() for line in f.readlines()]
