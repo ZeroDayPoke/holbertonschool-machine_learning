5f7ca8dfb58cd3b7a62a3e413fac768eb48f0b44
Chris Stamper <cstampok@gmail.com>
2023-08-15 15:42:15 -0500

    test two

---
+++ supervised_learning/object_detection/2-yolo.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""Object Detection - 1. Yolo"""
+
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+class Yolo:
+    """
+    Yolo v3 algorithm to perform object detection.
+
+    Attributes:
+        model (Keras model): The Darknet Keras model.
+        class_names (list): A list of the class names for the model.
+        class_t (float): The box score threshold for the initial filtering.
+        nms_t (float): The IOU threshold for non-max suppression.
+        anchors (numpy.ndarray): The anchor boxes.
+    """
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = tf.keras.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f.readlines()]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """Documentation"""
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        for i, output in enumerate(outputs):
+            grid_height, grid_width, anchor_boxes, _ = output.shape
+
+            # Box coordinates
+            tx = output[..., 0:1]
+            ty = output[..., 1:2]
+            tw = output[..., 2:3]
+            th = output[..., 3:4]
+
+            box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
+            box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
+
+            box_confidences.append(box_confidence)
+            box_class_probs.append(box_class_prob)
+
+            for cy in range(grid_height):
+                for cx in range(grid_width):
+                    for b in range(anchor_boxes):
+                        pw, ph = self.anchors[i][b]
+
+                        bx = (1 / (1 + np.exp(-tx[cy, cx, b]))) + cx
+                        by = (1 / (1 + np.exp(-ty[cy, cx, b]))) + cy
+                        bw = pw * np.exp(tw[cy, cx, b])
+                        bh = ph * np.exp(th[cy, cx, b])
+
+                        bx /= grid_width
+                        by /= grid_height
+                        bw /= int(self.model.input.shape[1])
+                        bh /= int(self.model.input.shape[2])
+
+                        x1 = (bx - (bw / 2)) * image_size[1]
+                        y1 = (by - (bh / 2)) * image_size[0]
+                        x2 = (bx + (bw / 2)) * image_size[1]
+                        y2 = (by + (bh / 2)) * image_size[0]
+
+                        tx[cy, cx, b] = x1
+                        ty[cy, cx, b] = y1
+                        tw[cy, cx, b] = x2
+                        th[cy, cx, b] = y2
+
+            boxes.append(np.concatenate((tx, ty, tw, th), axis=-1))
+
+        return (boxes, box_confidences, box_class_probs)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """Filter box outputs."""
+        box_scores = []
+        for i in range(len(boxes)):
+            box_scores.append(box_confidences[i] * box_class_probs[i])
+        
+        # Getting the box classes and their respective scores
+        box_classes = [np.argmax(box_score, axis=-1) for box_score in box_scores]
+        box_class_scores = [np.max(box_score, axis=-1) for box_score in box_scores]
+        
+        # Filtering the boxes
+        prediction_mask = [box_class_score >= self.class_t for box_class_score in box_class_scores]
+
+        # Using the mask to filter out boxes, their classes and scores
+        filtered_boxes = [box[prediction_mask[i]] for i, box in enumerate(boxes)]
+        box_classes = [box_class[prediction_mask[i]] for i, box_class in enumerate(box_classes)]
+        box_scores = [box_class_score[prediction_mask[i]] for i, box_class_score in enumerate(box_class_scores)]
+
+        # Flatten the filtered boxes, classes and scores
+        filtered_boxes = np.concatenate(filtered_boxes)
+        box_classes = np.concatenate(box_classes)
+        box_scores = np.concatenate(box_scores)
+
+        return filtered_boxes, box_classes, box_scores
